ba414fae9f032b0a6411c3f8e1384b8123be0a22
Itsss-shub <shubhum1712@gmail.com>
2024-06-20 00:29:39 +0530

    s

--- CMms/src/main/java/com/bank/homeloan/restapi/app/model/SanctionLetter.java
+++ CMms/src/main/java/com/bank/homeloan/restapi/app/model/SanctionLetter.java
@@ -22,19 +22,20 @@ public class SanctionLetter{
 	@Id
 	@GeneratedValue(strategy = GenerationType.AUTO)
 	private Integer sanctionLetterId;
+	
     private Date sanctionDate;
     private String applicantName;
     private String contact;
     private Double loanAmountSanctioned;
-    
+    //ch
     private String interestType;
     private Double rateOfInterest;
-    
+    //ch
     private Integer loanTenure;
     private Double monthlyEmiAmount;
     private String modeOfPayment;
 
-    
+    //add
     @Lob
 	@Column(length = 9000000)
 	private byte[] sanctionLetter;
